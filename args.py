0da2f7ef51915c6487ddb8d7f3b6e89806cd9ddb
NikaMasa51 <48128380+NikaMasa51@users.noreply.github.com>
2021-03-02 10:51:32 +0900

    change args
    change args

--- args.py
+++ args.py
@@ -1,7 +1,7 @@
 # img_dir = './data/segmentation_full/images'
 # mask_dir ='./data/segmentation_full/masks'
-img_dir = '../data/segmentation_sub/images'
-mask_dir ='../data/segmentation_sub/masks'
+img_dir = './data/segmentation_sub/images'
+mask_dir ='./data/segmentation_sub/masks'
 out_dir = './runs'
 n_epochs = 100
 n_classes = 2
@@ -12,4 +12,4 @@ batch_size = 8
 lr = 0.002
 beta_1 = 0.5
 beta_2 = 0.999
-im_size = 1024
+im_size = 256
